1b58ef45c65f1c39c68d812bdf22170b8bb7697b
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2019-07-17 14:12:53 -0500

    Added to the dataloader so that now it returns labels

--- loader/DataLoader.py
+++ loader/DataLoader.py
@@ -16,39 +16,63 @@ from DataParameters import PARAMETERS as params
 
 
 class RoseEtudes(Dataset):
+    '''Data loader class for reading the Rose Etude data from the .h5 file stored in path.
+
+    Args: path
+        path (string): The location of the Rose Etudes .h5 files.
+        data_name (string): Name of the Rose Etudes data .h5 file.
+        labels_name (string): Name of the Rose Etudes label .h5 file.
+    '''
+    def __init__(self, path, data_name, labels_name):
+        self.rose_data_frame = h5py.File(path + data_name, 'r')
+        self.rose_data_keys = list(self.rose_data_frame.keys())
+        self.rose_labels_frame = h5py.File(path + labels_name, 'r')
+        self.rose_labels_keys = list(self.rose_labels_frame.keys())
+        # the number of frames to include from the file
+        self.num_frames = int(params['sound_duration'] * 44100)
+
+    def __len__(self):
+        return len(self.rose_data_keys)
+
+    def __getitem__(self, idx):
+        rose_data = torch.from_numpy(
+            self.rose_data_frame[self.rose_data_keys[idx]][:self.num_frames])
+        rose_labels = self.rose_labels_frame[self.rose_labels_keys[idx]][:, 3:5]
+        rose_labels = torch.tensor([self.name_to_midi(note, octave) for note, octave in
+                                    zip(rose_labels[:, 0], rose_labels[:, 1])])
+        return rose_data, rose_labels
+    def name_to_midi(self, note, octave):
+        '''Method for converting between note names and midi labels
+
+        Input: note, octave
+            note (string): The name of the note to be converted to midi.
+            octave (int): The octave of the note to be converted to midi.
+
+        Output: midi
+            midi (int): The midi note corresponding to the input.
+        '''
+        name = {b'rest': 0,
+                b'C-': -1, b'C': 0, b'C#': 1, b'C##': 2,
+                b'D-': 1, b'D': 2, b'D#': 3,
+                b'E-':3, b'E': 4, b'E#': 5,
+                b'F-': 4, b'F': 5, b'F#': 6, b'F##': 7,
+                b'G-': 6, b'G': 7, b'G#': 8, b'G##': 9,
+                b'A-': 8, b'A': 9, b'A#': 10,
+                b'B--': 9, b'B-': 10, b'B': 11, b'B#': 12}
+        midi = name[note] + (int(octave) + 1) * 12
+        return midi
+
+
+class Philharmonia(Dataset):
     '''Data loader class for reading the Philharmonia data from the .h5 file stored in path.
 
     Args: path
         path (string): The location of the Philharmonia .h5 file.
         name (string): Name of the Philharmonia .h5 file.
     '''
-
-    def __init__(self, path, name):
-        self.rose_frame = h5py.File(path + name, 'r')
-        self.rose_keys = list(self.rose_frame.keys())
-        # the number of frames to include from the file
-        self.num_frames = int(params['sound_duration'] * 44100)
-
-    def __len__(self):
-        return len(self.rose_keys)
-
-    def __getitem__(self, idx):
-        rose_data = torch.from_numpy(
-            self.rose_frame[self.rose_keys[idx]][:self.num_frames])
-        return rose_data
-
-
-class Philharmonia(Dataset):
-    '''Data loader class for reading the Rose Etude data from the .h5 file stored in path.
-
-    Args: path
-        path (string): The location of the Rose Etudes .h5 file.
-        name (string): Name of the Rose Etudes .h5 file.
-    '''
-
     def __init__(self, path, name):
         self.phil_frame = h5py.File(path + name, 'r')
-        phil_keys = list(self.phil_frame.keys())
+        phil_keys = np.array(list(self.phil_frame.keys()))
         # shuffle the keys so as to not bias the input data
         random.Random(4).shuffle(phil_keys)
         '''
@@ -72,8 +96,8 @@ class Philharmonia(Dataset):
         self.phil_keys = phil_keys[useful_samples]
         self.information = information[useful_samples]
         # the labels are the note names
-        self.labels = torch.from_numpy(
-            self.information[:, 1].astype(int)).long()
+        self.labels = torch.tensor([
+            self.name_to_midi(info) for info in self.information[:, 1]]).long()
 
     def __len__(self):
         return len(self.phil_keys)
@@ -81,11 +105,22 @@ class Philharmonia(Dataset):
     def __getitem__(self, idx):
         phil_data = torch.from_numpy(
             self.phil_frame[self.phil_keys[idx]][:]).float()
+        phil_labels = self.labels[idx].long()
+        return phil_data, phil_labels
+    def name_to_midi(self, note):
+        '''Method for converting note name labels to midi labels
+        Input: note
+            note (string): Note name to convert to midi
 
-        return phil_data
+        Output: midi
+            midi (int): output midi note
+        '''
+        note_names = 'C Cs D Ds E F Fs G Gs A As B'.split(' ')
+        midi = (note_names.index(note[:-1]))+(int(note[-1])+1)*12
+        return midi
 
-DATASETS = {'Rose Etudes': RoseEtudes('../data/audio_data/', 'Rose.h5'),
-            'Philharmonia': Philharmonia('../data/audio_data', 'Phil.h5')}
+DATASETS = {'Rose Etudes': RoseEtudes('../data/audio_data/', 'Rose_Data.h5', 'Rose_Labels.h5'),
+            'Philharmonia': Philharmonia('../data/audio_data/', 'Phil.h5')}
 
 
 def get_loader(dataset, batch_size=1, shuffle=False,
